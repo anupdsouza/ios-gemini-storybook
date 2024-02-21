02a718b7371a27394fea3e6cd67f272c671233e5
Anup DSouza <anup.dsouza@gmail.com>
2024-02-21 16:27:35 +0530

    Add initial files

---
+++ Storybook/Storybook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Storybook
+//
+//  Created by Anup D'Souza on 21/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Storybook/Storybook/StorybookApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorybookApp.swift
+//  Storybook
+//
+//  Created by Anup D'Souza on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct StorybookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
