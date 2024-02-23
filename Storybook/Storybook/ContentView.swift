8b1534b71b35bd9627769748a943be5b9a36b21f
Anup DSouza <anup.dsouza@gmail.com>
2024-02-23 15:41:13 +0530

    Update home screen ui

--- Storybook/Storybook/ContentView.swift
+++ Storybook/Storybook/ContentView.swift
@@ -9,73 +9,105 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var currentItemID: UUID?
-    @State private var currentIndex: Int = 0
     private let images = ["poster1","poster2","poster3","poster4","poster5","poster6"]
     
     var body: some View {
-        VStack(spacing: 0) {
+        VStack {
             
+            // MARK: Top components
             Group {
-
                 HStack {
                     Spacer()
                     Image(systemName: "gearshape")
                         .font(.title)
+                        .foregroundStyle(.white)
                 }
-                
                 HStack {
                     Text("My Stories")
                         .font(.title).bold()
+                        .foregroundStyle(.white)
                     Spacer()
                     Button("Shelves", systemImage: "books.vertical.fill") {
                         
                     }
                     .buttonStyle(.borderedProminent)
                     .buttonBorderShape(.capsule)
-                    .tint(.gray)
+                    .tint(.secondary)
                 }
+                .bold()
             }
             .padding(.horizontal, 20)
-            .padding(.vertical, 10)
+            .padding(.vertical, 15)
             
-            ScrollView(.horizontal) {
+            // MARK: Carousal
+            ScrollView(.horizontal, showsIndicators: false) {
                 LazyHStack(spacing: 0) {
                     ForEach(0..<images.count, id: \.self) { index in
                         Image(images[index])
                             .resizable()
                             .scaledToFill()
-                            .clipShape(RoundedRectangle(cornerRadius: 25.0))
-                            .padding(.horizontal, 25)
+                            .frame(width: 260, height: 380)
+                            .clipShape(RoundedRectangle(cornerRadius: 20.0))
                             .id(index)
                             .containerRelativeFrame(.horizontal)
                             .scrollTransition { content, phase in
                                 content
-                                    .scaleEffect(phase.isIdentity ? 1 : 0.8)
+                                    .scaleEffect(phase.isIdentity ? 1 : 0.9)
                             }
                     }
                 }
                 .scrollTargetLayout()
             }
-            .frame(height: 350)
-            .padding(.vertical)
-            .scrollIndicators(.never)
+            .frame(height: 400)
+            .contentMargins(20)
+            .safeAreaPadding(.horizontal, 40)
             .scrollTargetBehavior(.viewAligned)
             .scrollPosition(id: $currentItemID)
-            .safeAreaPadding(.horizontal, 80)
-            .safeAreaPadding(.vertical, 10)
             
             Text("Visiting Mr. Freeze")
                 .font(.title).bold()
+                .foregroundStyle(.white)
             
-            Button("New Story", systemImage: "plus") {
+            // MARK: Bottom components
+            Group {
+                HStack(spacing: 30) {
+                    Button(action: {}, label: {
+                        Image(systemName: "arrow.down.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40)
+                    })
+                    Button(action: {}, label: {
+                        Image(systemName: "ellipsis.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40)
+                    })
+                }
                 
+                Button(action: {}, label: {
+                    HStack(spacing: 10) {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 15)
+                        Text("New Story")
+                    }
+                    .bold()
+                    .padding(5)
+                    .foregroundStyle(.black)
+                })
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+                .padding()
             }
-            .padding()
-            .buttonStyle(.borderedProminent)
-            .buttonBorderShape(.capsule)
-            .tint(.black)
+            .tint(.white)
 
             Spacer()
+        }
+        .background {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
         }
     }
 }
