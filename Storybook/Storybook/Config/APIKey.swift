063f2884d788f23f593d460e424110cfa7d9da78
Anup DSouza <anup.dsouza@gmail.com>
2024-03-18 18:27:27 +0530

    Update APIKey.swift

--- Storybook/Storybook/Config/APIKey.swift
+++ Storybook/Storybook/Config/APIKey.swift
@@ -9,51 +9,35 @@ import Foundation
 
 enum APIKey {
     // Fetch the API keys from `GenerativeAI-Info.plist`
-    static var `gemini`: String {
+    static var gemini: String {
+        configValue(key: "GEMINI_API_KEY",
+                    setupErrorMessage: "Follow the instructions at https://ai.google.dev/tutorials/setup to get an API key.")
+    }
+
+    static var openAIKey: String {
+        configValue(key: "OPENAI_API_KEY",
+                    setupErrorMessage: "Visit https://platform.openai.com/api-keys to get an API key.")
+    }
+    
+    static var openAIOrgId: String {
+        configValue(key: "OPENAI_ORG_ID",
+                    setupErrorMessage: "Visit https://platform.openai.com/api-keys to see your organisation id.")
+    }
+
+    static private var configDict: NSDictionary? {
         guard let filePath = Bundle.main.path(forResource: "GenerativeAI-Info", ofType: "plist")
         else {
             fatalError("Couldn't find file 'GenerativeAI-Info.plist'.")
         }
-        let plist = NSDictionary(contentsOfFile: filePath)
-        guard let value = plist?.object(forKey: "GEMINI_API_KEY") as? String else {
-            fatalError("Couldn't find key 'GEMINI_API_KEY' in 'GenerativeAI-Info.plist'.")
+        return NSDictionary(contentsOfFile: filePath)
+    }
+
+    static private func configValue(key: String, setupErrorMessage: String) -> String {
+        guard let value = configDict?.object(forKey: key) as? String else {
+            fatalError("Couldn't find key: \(key) in 'GenerativeAI-Info.plist'.")
         }
         if value.starts(with: "_") || value.isEmpty {
-            fatalError(
-                "Follow the instructions at https://ai.google.dev/tutorials/setup to get an API key."
-            )
-        }
-        return value
-    }
-    static var openAIKey: String {
-        guard let filePath = Bundle.main.path(forResource: "GenerativeAI-Info", ofType: "plist")
-        else {
-            fatalError("Couldn't find file 'GenerativeAI-Info.plist'.")
-        }
-        let plist = NSDictionary(contentsOfFile: filePath)
-        guard let value = plist?.object(forKey: "OPENAI_API_KEY") as? String else {
-            fatalError("Couldn't find key 'OPENAI_API_KEY' in 'GenerativeAI-Info.plist'.")
-        }
-        if value.isEmpty {
-            fatalError(
-                "Visit https://platform.openai.com/api-keys to get an API key."
-            )
-        }
-        return value
-    }
-    static var openAIOrgId: String {
-        guard let filePath = Bundle.main.path(forResource: "GenerativeAI-Info", ofType: "plist")
-        else {
-            fatalError("Couldn't find file 'GenerativeAI-Info.plist'.")
-        }
-        let plist = NSDictionary(contentsOfFile: filePath)
-        guard let value = plist?.object(forKey: "OPENAI_ORG_ID") as? String else {
-            fatalError("Couldn't find key 'OPENAI_ORG_ID' in 'GenerativeAI-Info.plist'.")
-        }
-        if value.isEmpty {
-            fatalError(
-                "Visit https://platform.openai.com/api-keys to see your organisation id."
-            )
+            fatalError(setupErrorMessage)
         }
         return value
     }
